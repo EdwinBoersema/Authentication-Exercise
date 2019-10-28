71d3c0dd4d2b05d0c91cda40d09089703d5bfbe7
EdwinBoersema <edwinboersema@gmail.com>
2019-10-28 15:01:55 +0100

    Added the option to log in.

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const
     mongoose = require("mongoose"),
     passport = require("passport"),
     bodyParser = require("body-parser"),
-    User = require("./models/user"),
     localStrategy = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose");
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user");
 
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
     secret: "Flaters",
     resave: false,
     saveUninitialized: false
 }));
-app.use(passport.initialize);
+app.use(passport.initialize());
 app.use(passport.session());
-
-app.listen(3000, () => console.log("Server started!"));
 
 mongoose.connect("mongodb://localhost:27017/Auth_demo", {
     useNewUrlParser: true,
@@ -27,8 +26,10 @@ mongoose.connect("mongodb://localhost:27017/Auth_demo", {
     () => console.log("Connected to database")
 );
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 
 // =================
 // ROUTES
@@ -44,13 +45,43 @@ app.get("/secret", (req, res) => {
     res.render("secret");
 });
 
+//=================
 // Auth Routes
 
 // show sign up form
 app.get("/register", (req, res) => {
-    if (err) {
-        console.log(err);
-    } else {
-        res.render("register");
-    }
+    res.render("register");
 });
+
+// handling user sign up
+app.post("/register", (req, res) => {
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, () =>{
+                res.redirect("/secret");
+            });
+        }
+    });
+});
+
+//==============
+// LOGIN Routes
+
+// render the login form
+app.get("/login", (req, res) =>{
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), (req, res) => {
+    
+});
+
+app.listen(3000, () => console.log("Server started!"));
